11b820d4f07a802eb1c73628857f3bbe1c4a2bc1
SwanyCastle <seunghwan712@gmail.com>
2024-11-10 05:37:47 +0900

    [Feat]: QueryDSL 사용을 위한 설정
    QueryDSL 사용을 위한 설정
    - build.gradle 에 QueryDSL 사용을 위해 필요한 라이브러리 추가
    - Spring Boot 환경에서 QueryDSL 을 사용하기 위해 QueryDslConfig 파일 생성
    - ReviewCustomRepository 와 그 구현체인 ReviewCustomRepositoryImpl 을 생성하고
      ReviewCustomRepositoryImpl 안에 JPAQueryFactory 을 주입받아 필요한 쿼리를 생성
      ReviewRepository 에서 ReviewCustomRepository 상속받아
      ReviewCustomRepositoryImpl 에서 구현한 쿼리를 사용

---
+++ src/main/java/com/ecommerce/repository/review/ReviewRepository.java
@@ -0,0 +1,10 @@
+package com.ecommerce.repository.review;
+
+import com.ecommerce.entity.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Long>, ReviewCustomRepository{
+
+}
